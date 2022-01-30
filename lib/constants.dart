4d27f3ff9172a0ca403326b602aecf54cb261749
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2022-01-30 02:02:19 +0000

    :art: Tested web version

--- lib/constants.dart
+++ lib/constants.dart
@@ -23,3 +23,10 @@ String introMessage(String currentVersion) => '''
      ANIMATED NATIVE SPLASH (v$currentVersion)
   ════════════════════════════════════════════
   ''';
+
+///Web
+const String _webFolder = 'web/';
+const String _webSplashFolder = _webFolder + 'splash/';
+const String _webSplashImagesFolder = _webSplashFolder + 'img/';
+const String _webIndex = _webFolder + 'index.html';
+const String _webRelativeStyleFile = 'splash/style.css';
